508953b5cbd055997066c524221e390fe5c884c5
Lexus <rounin_lopez@yahoo.com>
2020-10-19 22:31:53 -0500

    added ajax to datale

--- Moon/App_Start/BundleConfig.cs
+++ Moon/App_Start/BundleConfig.cs
@@ -9,10 +9,10 @@ namespace Moon
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.5.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
